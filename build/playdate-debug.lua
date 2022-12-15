e1899f1b90fb7502d8f153bcea249a7b0ab90a11
Alex Larioza <contact@alexlarioza.com>
2022-12-15 11:28:33 -0800

    orgnaized tasks and build scripts

---
+++ build/playdate-debug.lua
@@ -0,0 +1,24 @@
+local build = require("playbit.build")
+
+build.build({ 
+  assert = true,
+  debug = true,
+  platform = "playdate",
+  output = "_pdx\\",
+  clearBuildFolder = true,
+  fileProcessors = {
+    lua = build.luaProcessor,
+    wav = build.waveProcessor,
+    aseprite = build.asepriteProcessor,
+  },
+  files = {
+    -- essential playbit files for love2d
+    { "playbit/playbit", "playbit" },
+    -- project
+    { "src/fonts", "fonts" },
+    { "src/textures", "textures" },
+    { "src/sounds", "sounds" },
+    { "src/main.lua", "main.lua" },
+    { "src/pdxinfo.txt", "pdxinfo" }, -- .txt is work around for this being detected as folder
+  },
+})
